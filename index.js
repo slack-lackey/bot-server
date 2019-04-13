b908a32785b6654e7da5d6424c3d6f8cdb882a10
Billy Bunn <williammbunn@gmail.com>
2019-04-13 14:30:11 -0700

    adds example code

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+// Creates express app
+const app = express();
+// The port used for Express server
+const PORT = 3000;
+// Starts server
+app.listen(process.env.PORT || PORT, function() {
+  console.log('Bot is listening on port ' + PORT);
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.post('/test', (req, res) => {
+  var data = {form: {
+    token: process.env.SLACK_AUTH_TOKEN,
+    channel: '#test',
+    text: `Hi! :wave: I'm your new bot.`,
+  }};
+  request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
+    // Sends welcome message
+    res.json();
+  });
+});
